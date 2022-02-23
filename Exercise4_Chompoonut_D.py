2062c953ac1440028c35c223909a784480e4edd8
Popdelay <chompoonut.det@gmail.com>
2022-02-23 17:08:49 +0700

    Delete the problem
    ลบโจทย์ออกจากไฟล์

---
+++ Exercise4_Chompoonut_D.py
@@ -0,0 +1,13 @@
+FE = 7.5
+GB = 8.0
+TCS = 9.0
+CP = 6.0
+
+print("-------- Your Score --------")
+print("Foundation English: ", FE, "คะแนน")
+print("General Business: ", GB, "คะแนน")
+print("Introduction to Computer Systems: ", TCS, "คะแนน")
+print("Computer Programming:", CP, "คะแนน")
+print("-----------------------------")
+print("Total: ", FE+GB+TCS+CP, "คะแนน")
+
